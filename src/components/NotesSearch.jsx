67a5e8703fdc384871e7b078193ad06aa50d0d65
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2022-07-02 09:11:02 +0700

    change NotesSearch to funciton component

--- src/components/NotesSearch.jsx
+++ src/components/NotesSearch.jsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class NotesSearch extends Component {
-	constructor(props) {
-		super(props);
-
-		this.onQueryChange = this.onQueryChange.bind(this);
-	}
-
-	onQueryChange(event) {
-		this.props.onSearch(event.target.value);
-	}
-
-	render() {
-		return (
-			<div className="note-search">
-				<label htmlFor="input__search">Search note</label>
-				<input
-					id="input__search"
-					type="text"
-					onChange={this.onQueryChange}
-				></input>
-			</div>
-		);
-	}
+export default function NotesSearch({ onSearch }) {
+	return (
+		<div className="note-search">
+			<label htmlFor="input__search">Search note</label>
+			<input
+				id="input__search"
+				type="text"
+				onChange={(event) => onSearch(event.target.value)}
+			></input>
+		</div>
+	);
 }
-
-export default NotesSearch;
